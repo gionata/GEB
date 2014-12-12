99b80a60f8ad1228826efa097a93a7a80d4b6a83
Gionata <gionata@users.noreply.github.com>
2014-12-12 18:57:42 +0100

    Fix: hyphens_leq when the number of hyphen is less of 0
    And a new example.

--- chapter_03/composite.pl
+++ chapter_03/composite.pl
@@ -23,26 +23,27 @@ c(Z) :-
 % Composite number, generator
 c(Z) :-
     var(Z),
-    % generate
+    % generate the multipliers
     hyphens(X),
     hyphens(Y),
     length(X, LX),
     length(Y, LY),
     LZ is (LX + 1) * (LY + 1),
-    hyphens_leq(Z, LZ),
+    % generate only string having a reasonable number of hyphens, unless the interpreter can't manage the code
+    hyphens_eq(Z, LZ),
     % apply the definition
     phrase((seq(X),seq([-,t]),seq(Y),seq([-,q]),seq(Z)), String),
     tq(String),
     write(String), nl.
 
-
-% Accepted symbols
+% Strings of hyphens
 hyphens([-]).
 hyphens([-|Rest]) :-
     hyphens(Rest).
 
-
-hyphens_leq(_, 0) :-
+% Generate a string of, at most, Remaining hyphens
+hyphens_leq(_, Remaining) :-
+    Remaining =< 0,
     !,
     fail.
 hyphens_leq([-], Remaining) :-
@@ -51,15 +52,12 @@ hyphens_leq([-|Rest], Remaining) :-
     M is Remaining - 1,
     hyphens_leq(Rest, M).
 
-accepted_symbol(t).
-accepted_symbol(q).
-accepted_symbol(-).
-
-% Recognize only legal strings.
-legal_string([]).
-legal_string([Letter|Rest]) :-
-	accepted_symbol(Letter), !,
-	legal_string(Rest).
+% Generate a string of Remaining hyphens
+hyphens_eq([-], 1) :- !.
+hyphens_eq(X, Remaining) :-
+    X = [-|Rest],
+    M is Remaining - 1,
+    hyphens_eq(Rest, M).
 
 % Axioms:  Xt-qX is an axiom, whenever X is composed of hyphens only.
 tq(String) :-
@@ -84,5 +82,6 @@ seq([E|Es]) --> [E], seq(Es).
 ?- c([-,-,-]).
 ?- c([-,-,-,-]).
 ?- c([-,-,-,-,-,-,-,-,-,-,-]).
+?- c(X).
 */
 
